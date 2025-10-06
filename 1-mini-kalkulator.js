aad6ee7e9b9dab486eadeaf862935a0b3554446d
hafizalqaiyi <hafizalqaiyim@gmail.com>
2025-10-06 08:05:42 +0700

    Add files via upload

---
+++ 1-mini-kalkulator.js
@@ -0,0 +1,54 @@
+const tambah = (S,T) => {
+    S = 30
+    T = 20
+    console.log (S + T)
+
+}
+
+tambah();
+
+
+const perkalian = (S,T) => {
+    S = 30
+    T = 20
+    console.log (S * T)
+
+}
+
+perkalian();
+
+const pengurangan = (S,T) => {
+    S = 30
+    T = 20
+    console.log (S - T)
+
+}
+
+pengurangan();
+
+const pembagian = (S,T) => {
+    S = 30
+    T = 20
+    console.log (S / T)
+
+}
+
+pembagian();
+
+const modulus = (S,T) => {
+    S = 30
+    T = 20
+    console.log (S % T)
+
+}
+
+modulus();
+
+const perpangkatan = (S,T) => {
+    S = 30
+    T = 20
+    console.log (S ** T)
+
+}
+
+perpangkatan();
